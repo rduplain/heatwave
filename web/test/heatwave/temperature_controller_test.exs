bc167b20fa55f8a5796098eea2ddaa283bb55243
Rom DuPlain <rom.duplain@gmail.com>
2025-08-24 15:23:10 -0700

    Format.

--- web/test/heatwave/temperature_controller_test.exs
+++ web/test/heatwave/temperature_controller_test.exs
@@ -23,34 +23,45 @@ defmodule Heatwave.TemperatureControllerTest do
     test "create_temperature/1 with valid data creates a temperature" do
       valid_attrs = %{value: 120.5, sensor: "some sensor"}
 
-      assert {:ok, %Temperature{} = temperature} = TemperatureController.create_temperature(valid_attrs)
+      assert {:ok, %Temperature{} = temperature} =
+               TemperatureController.create_temperature(valid_attrs)
+
       assert temperature.value == 120.5
       assert temperature.sensor == "some sensor"
     end
 
     test "create_temperature/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = TemperatureController.create_temperature(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} =
+               TemperatureController.create_temperature(@invalid_attrs)
     end
 
     test "update_temperature/2 with valid data updates the temperature" do
       temperature = temperature_fixture()
       update_attrs = %{value: 456.7, sensor: "some updated sensor"}
 
-      assert {:ok, %Temperature{} = temperature} = TemperatureController.update_temperature(temperature, update_attrs)
+      assert {:ok, %Temperature{} = temperature} =
+               TemperatureController.update_temperature(temperature, update_attrs)
+
       assert temperature.value == 456.7
       assert temperature.sensor == "some updated sensor"
     end
 
     test "update_temperature/2 with invalid data returns error changeset" do
       temperature = temperature_fixture()
-      assert {:error, %Ecto.Changeset{}} = TemperatureController.update_temperature(temperature, @invalid_attrs)
+
+      assert {:error, %Ecto.Changeset{}} =
+               TemperatureController.update_temperature(temperature, @invalid_attrs)
+
       assert temperature == TemperatureController.get_temperature!(temperature.id)
     end
 
     test "delete_temperature/1 deletes the temperature" do
       temperature = temperature_fixture()
       assert {:ok, %Temperature{}} = TemperatureController.delete_temperature(temperature)
-      assert_raise Ecto.NoResultsError, fn -> TemperatureController.get_temperature!(temperature.id) end
+
+      assert_raise Ecto.NoResultsError, fn ->
+        TemperatureController.get_temperature!(temperature.id)
+      end
     end
 
     test "change_temperature/1 returns a temperature changeset" do
